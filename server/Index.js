8042669fb39d86c33cd8466227db282d73383040
Lavesh Khairajani <115465151+LaveshK11@users.noreply.github.com>
2023-09-19 21:16:53 +0530

    change in formatting of code

--- server/Index.js
+++ server/Index.js
@@ -6,9 +6,11 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 
 const app = express();
+
 app.use(cors());
 
 app.use(express.json());
+
 app.use(
   bodyParser.urlencoded({
     extended: true,
